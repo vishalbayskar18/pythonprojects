e4d6a7349dae0bdd4be4e691ac58dad0ad1eb2c1
vibyask6 <vibyask6@in.ibm.com>
2021-01-05 09:43:19 +0530

    modified app.py and added requirement

--- universe/app.py
+++ universe/app.py
@@ -1,1 +1,14 @@
-print ("Welcome to my universe ...")
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    print ("Welcome to my universe ...")
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run()
+
+
+
