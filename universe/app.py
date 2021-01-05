542e1a241027c47458965fc3bfb1d6d3b963de1b
vishalbayskar18 <45871181+vishalbayskar18@users.noreply.github.com>
2021-01-05 14:59:29 +0530

    Update app.py

--- universe/app.py
+++ universe/app.py
@@ -7,8 +7,11 @@ def hello():
     print ("Welcome to my universe ...")
     return "Hello World!"
 
+@app.route('/hi')
+def hi():
+    return "Hi How Are you"
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=8080)
 
 
 
